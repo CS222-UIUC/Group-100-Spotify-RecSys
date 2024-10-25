6d080b0a60b106f2aa30e0018666bb04570ae84d
mpdatt <mpdatt2@illinois.edu>
2024-10-25 10:10:53 -0500

    exception, input, edgecase handling

--- spotify_requests/geniusapi.py
+++ spotify_requests/geniusapi.py
@@ -1,44 +1,51 @@
-import requests
-import base64
-import datetime
-from urllib.parse import urlencode
-from flask import Flask, url_for, jsonify, request, redirect
-import spotipy 
-from spotipy.oauth2 import SpotifyClientCredentials,SpotifyOAuth
+
+import spotipy
+from spotipy.oauth2 import SpotifyOauthError
 from lyricsgenius import Genius
 import sys
 import os
-import json
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
 from constants import *
 
-#if __name__ == '__main__':
-    
-    #get spotify authorization and access token with necessary scope
-def GetAPIObjects():
+# get spotify authorization and access token with necessary scope
+def GetAccessTokenWithAuthorization():
     scope = 'user-top-read'
-    oauth_object = spotipy.SpotifyOAuth(client_id= SPOTIFY_API_CLIENT_ID,
-                                    client_secret= SPOTIFY_API_CLIENT_SECRET,
+    try:
+        oauth_object = spotipy.SpotifyOAuth(client_id=SPOTIFY_API_CLIENT_ID,
+                                    client_secret=SPOTIFY_API_CLIENT_SECRET,
                                     redirect_uri=SPOTIPY_API_REDIRECT_URI,
-                                    scope = scope,
-                                )
-    token = oauth_object.get_access_token(as_dict=False)
-    spotify = spotipy.Spotify(auth=token)
+                                    scope=scope,
+                                    )
+        token = oauth_object.get_access_token(as_dict=False)
+        return token
+    except SpotifyOauthError:
+        print("Authorization failed")
+
+# get APIs
+def GetAPIObjects(token):
+    try:
+        spotify = spotipy.Spotify(auth=token)
+    except spotipy.client.SpotifyException:
+        return "bad authtoken"
     genius = Genius(GENIUS_API_CLIENT_ACCESS_TOKEN)
     return spotify, genius
 
 def RecommendationsFromTops():
-    spotify,genius = GetAPIObjects()
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
 
-    #get user's top tracks and artists
-    top_track_response = spotify.current_user_top_tracks(limit=3)
-    top_artist_response = spotify.current_user_top_artists(limit=2)
+    # get user's top tracks and artists
+    try:
+        top_track_response = spotify.current_user_top_tracks(limit=3)
+        top_artist_response = spotify.current_user_top_artists(limit=2)
+    except spotipy.client.SpotifyException:
+        print("Error with top retrieval")
 
-    #get top artist ids and top song names/ids
-    top_artists= []
+    # get top artist ids and top song names/ids
+    top_artists = []
     top_names = []
     top_ids = []
     for i in range(len(top_artist_response["items"])):
@@ -48,7 +55,7 @@ def RecommendationsFromTops():
         top_names.append(top_track_response["items"][i]["name"])
         top_ids.append(top_track_response["items"][i]["id"])
 
-    #get recommendations
+    # get recommendations
     recs_response = spotify.recommendations(seed_artists=top_artists, seed_genres=None, seed_tracks=top_ids, limit=1, country=None)
     recs = []
     for i in range(len(recs_response['tracks'])):
@@ -56,8 +63,14 @@ def RecommendationsFromTops():
     return recs
 
 def CommentsofRecommendations(recs):
-    spotify,genius = GetAPIObjects()
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
+    if(type(recs) != list):
+        raise TypeError
     song = genius.search_song(recs[0])
+    print(song)
+    if(song is None):
+        return "song not found"
 
     comments_request = genius.song_comments(song.id, per_page=10)
     comments = []
@@ -67,14 +80,15 @@ def CommentsofRecommendations(recs):
 
     return comments
 
-#FailedExperiment, will not test bc will not be used
+# FailedExperiment, will not test bc will not be used
 def RecommendationsFromAnnotations(song):
-    spotify,genius = GetAPIObjects()
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
     annotations = genius.song_annotations(song_id=song.id)
 
-    #get recommendations by running annotations through lyric search
+    # get recommendations by running annotations through lyric search
     recs = []
     for i in range(len(annotations)):
-        search_request  = genius.search_lyrics(annotations[i][1][0][0],per_page=1)
+        search_request = genius.search_lyrics(annotations[i][1][0][0], per_page=1)
         if(len(search_request['sections'][0]['hits']) != 0):
             recs.append(search_request['sections'][0]['hits'][0]['result']['title'])
--- spotify_requests/test_apifunctions.py
+++ spotify_requests/test_apifunctions.py
@@ -1,18 +1,25 @@
 import unittest
 
-from geniusapi import GetAPIObjects
-from geniusapi import RecommendationsFromTops
-from geniusapi import CommentsofRecommendations
-
-
+from geniusapi import GetAPIObjects, RecommendationsFromTops, CommentsofRecommendations, GetAccessTokenWithAuthorization
+import warnings
 
 class TestRecommendationAndComments(unittest.TestCase):
+
+    def test_getAccessTokenWithAuthorization(self):
+        """
+        Test that function runs without error
+        """
+        try:
+            GetAccessTokenWithAuthorization()
+        except BaseException as exc:
+            assert False, f'raised an exception {exc}'
     def test_getAPI(self):
         """
         Test that function runs without error
         """
         try:
-            genius, spotify = GetAPIObjects()
+            token = GetAccessTokenWithAuthorization()
+            spot, gen = GetAPIObjects(token)
         except BaseException as exc:
             assert False, f'raised an exception {exc}'
     def test_recommendations(self):
@@ -20,16 +27,33 @@ class TestRecommendationAndComments(unittest.TestCase):
         Test that function runs without error
         """
         try:
-            recs = RecommendationsFromTops()
+            RecommendationsFromTops()
         except BaseException as exc:
             assert False, f'raised an exception {exc}'
-    def test_getComments(self):
+    def test_CommentsWeirdInput(self):
         """
         Test that function only accepts list
         """
         data = 8
         with self.assertRaises(TypeError):
-            result = CommentsofRecommendations(data)
+            CommentsofRecommendations(data)
+    
+    def test_CommentsStrInput(self):
+        """
+        Test that function only accepts list
+        """
+        data = "RipTide"
+        with self.assertRaises(TypeError):
+            CommentsofRecommendations(data)
+
+    def test_CommentsNoSong(self):
+        """
+        Test that function only accepts list
+        """
+        data = ["i6ri8buy"]
+        result = CommentsofRecommendations(data)
+        self.assertEqual(result, "song not found")
 
 if __name__ == '__main__':
+    warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
     unittest.main()
