1ce0703154be4261e22909b61116f7546552f315
mpdatt <mpdatt2@illinois.edu>
2024-11-13 22:21:41 -0600

    new recommender and linking songs between apis

--- spotify_requests/geniusapi.py
+++ spotify_requests/geniusapi.py
@@ -14,7 +14,7 @@ from constants import *
 
 # get spotify authorization and access token with necessary scope
 def GetAccessTokenWithAuthorization():
-    #scope = 'user-top-read' 'streaming' 'user-read-email' 'user-read-private'
+    #scope ='user-top-read'
     try:
         oauth_object = spotipy.SpotifyOAuth(client_id=SPOTIFY_API_CLIENT_ID,
                                     client_secret=SPOTIFY_API_CLIENT_SECRET,
@@ -60,11 +60,44 @@ def RecommendationsFromTops():
     # get recommendations
     recs_response = spotify.recommendations(seed_artists=top_artists, seed_genres=None, seed_tracks=top_ids, limit=5, country=None)
     recs = []
+    rec_artists = []
     for i in range(len(recs_response['tracks'])):
         recs.append(recs_response['tracks'][i]['name'])
-    return recs
+        rec_artists.append(recs_response['tracks'][i]['artists'][0]['name'])
+    return recs, rec_artists
 
-def CommentsofRecommendations(recs):
+def RecommendationsFromParams(genres, danceability, popularity, tempo):
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
+
+    if(danceability > 1 or danceability < 0):
+        print("invalid danceability")
+        return "invalid danceability", ""
+    
+    if(popularity > 100 or popularity < 0):
+        print("invalid popularity")
+        return "invalid popularity", ""
+    
+    if(tempo > 200 or tempo < 60):
+        print("invalid tempo")
+        return "invalid tempo", ""
+    
+    #figure out genre check!!!!
+    #all_genres = spotify.recommendation_genre_seeds()
+
+    #may want sliders or textbox for user to choose most parameters and then checkboxes for genres
+    #get recommendations based on: genre, danceabilitiy(0-1), popularity(0-100), tempo(60-200BPM)
+    
+    recs_response = spotify.recommendations(seed_genres=genres, target_danceability=danceability, target_popularity=popularity, target_tempo=tempo,limit=5, country=None)
+    recs = []
+    rec_artists = []
+
+    for i in range(len(recs_response['tracks'])):
+        recs.append(recs_response['tracks'][i]['name'])
+        rec_artists.append(recs_response['tracks'][i]['artists'][0]['name'])
+    return recs, rec_artists
+
+def CommentsofRecommendations(recs, rec_artists):
     token = GetAccessTokenWithAuthorization()
     spotify, genius = GetAPIObjects(token)
     if(type(recs) != list):
@@ -72,7 +105,7 @@ def CommentsofRecommendations(recs):
     comments = []
     for i in range(len(recs)):
         comments.append([])
-        song = genius.search_song(recs[i])
+        song = genius.search_song(recs[i], rec_artists[i])
         if(song is None):
             return "song not found"
         comments_request = genius.song_comments(song.id, per_page=10)
@@ -81,21 +114,21 @@ def CommentsofRecommendations(recs):
     return comments
 
 #takes in recommendations and gives facts about them
-def InfoOfRecommendations(recs):
-    recs = ['Riptide', 'Everybody Talks', 'APT.']
-    #'Locked out of Heaven', 'Everybody Talks'
+def InfoOfRecommendations(recs, rec_artists):
     token = GetAccessTokenWithAuthorization()
     spotify, genius = GetAPIObjects(token)
-    if(type(recs) != list):
+    if(type(recs) != list or type(rec_artists) != list):
         raise TypeError
     for i in range(len(recs)):
-        song = genius.search_song(recs[i])
+        song = genius.search_song(recs[i], rec_artists[i])
         if(song is None):
             print ("song not found")
         song_data = genius.song(song.id)
         title = song_data['song']['full_title']
         artist = song_data['song']['artist_names']
         desc = song_data['song']['description_annotation']['annotations'][0]['body']['plain']
+        if(desc == ""):
+            desc = "No additional information available."
         print("___________________________________________________________________")
         print(title)
         print("Artist: " + artist)
@@ -116,3 +149,19 @@ def RecommendationsFromAnnotations(song):
         search_request = genius.search_lyrics(annotations[i][1][0][0], per_page=1)
         if(len(search_request['sections'][0]['hits']) != 0):
             recs.append(search_request['sections'][0]['hits'][0]['result']['title'])
+
+
+
+if __name__ == "__main__":
+   #I also made sure the song is connected to genius by both song name and author for security
+   #recommendations from attributes- can put in up to 5 genres
+    
+    genres = ['french','pop','r-n-b']
+    danceability = .9
+    popularity = 30
+    tempo = 130
+
+    recs, rec_artists = RecommendationsFromParams(genres, danceability, popularity, tempo)
+
+    InfoOfRecommendations(recs, rec_artists)
+
--- spotify_requests/test_apifunctions.py
+++ spotify_requests/test_apifunctions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from geniusapi import GetAPIObjects, RecommendationsFromTops, CommentsofRecommendations, GetAccessTokenWithAuthorization, InfoOfRecommendations
+from geniusapi import GetAPIObjects, RecommendationsFromTops, CommentsofRecommendations, GetAccessTokenWithAuthorization, InfoOfRecommendations, RecommendationsFromParams
 import warnings
 
 class TestRecommendationAndComments(unittest.TestCase):
@@ -38,7 +38,7 @@ class TestRecommendationAndComments(unittest.TestCase):
         """
         data = 8
         with self.assertRaises(TypeError):
-            CommentsofRecommendations(data)
+            CommentsofRecommendations(data, data)
     
     def test_CommentsStrInput(self):
         """
@@ -46,14 +46,14 @@ class TestRecommendationAndComments(unittest.TestCase):
         """
         data = "RipTide"
         with self.assertRaises(TypeError):
-            CommentsofRecommendations(data)
+            CommentsofRecommendations(data, data)
 
     def test_CommentsNoSong(self):
         """
         Test if song isnt real
         """
         data = ["i6ri8buy"]
-        result = CommentsofRecommendations(data)
+        result = CommentsofRecommendations(data, data)
         self.assertEqual(result, "song not found")
     
     def test_InfoWeirdInput(self):
@@ -62,7 +62,7 @@ class TestRecommendationAndComments(unittest.TestCase):
         """
         data = 8
         with self.assertRaises(TypeError):
-            InfoOfRecommendations(data)
+            InfoOfRecommendations(data, data)
     
     def test_InfoStrInput(self):
         """
@@ -70,15 +70,48 @@ class TestRecommendationAndComments(unittest.TestCase):
         """
         data = "RipTide"
         with self.assertRaises(TypeError):
-            InfoOfRecommendations(data)
+            InfoOfRecommendations(data, data)
 
     def test_InfoNoSong(self):
         """
         Test if song isnt real
         """
         data = ["i6ri8buy"]
-        result = InfoOfRecommendations(data)
+        result = InfoOfRecommendations(data,data)
         self.assertEqual(result, "song not found")
+    
+    def test_badDanceability(self):
+        """
+        Test function handles bad danceability input
+        """
+        genres = ['french','pop','r-n-b']
+        danceability = 1.4
+        popularity = 30
+        tempo = 100
+        result = RecommendationsFromParams(genres, danceability, popularity, tempo)
+        self.assertEqual(result, "invalid danceabliity")
+    
+    def test_badPopularity(self):
+        """
+        Test function handles bad popularity input
+        """
+        genres = ['french','pop','r-n-b']
+        danceability = .8
+        popularity = 300
+        tempo = 100
+        result = RecommendationsFromParams(genres, danceability, popularity, tempo)
+        self.assertEqual(result, "invalid popularity")
+    
+    def test_badTempo(self):
+        """
+        Test function handles bad tempo input
+        """
+        genres = ['french','pop','r-n-b']
+        danceability = .8
+        popularity = 30
+        tempo = 1000
+        result = RecommendationsFromParams(genres, danceability, popularity, tempo)
+        self.assertEqual(result, "invalid tempo")
 
 if __name__ == '__main__':
     warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
