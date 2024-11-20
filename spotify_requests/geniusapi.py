80bd8622948106e7cd36f2d094166e3fdad92c21
mpdatt <mpdatt2@illinois.edu>
2024-11-20 15:34:25 -0600

    user can save recommendations to liked or playlist

--- spotify_requests/geniusapi.py
+++ spotify_requests/geniusapi.py
@@ -14,12 +14,12 @@ from constants import *
 
 # get spotify authorization and access token with necessary scope
 def GetAccessTokenWithAuthorization():
-    #scope ='user-top-read'
+    scope = 'playlist-modify-public user-read-email user-read-private user-top-read playlist-read-private user-library-modify user-library-read playlist-modify-private'
     try:
         oauth_object = spotipy.SpotifyOAuth(client_id=SPOTIFY_API_CLIENT_ID,
                                     client_secret=SPOTIFY_API_CLIENT_SECRET,
                                     redirect_uri=SPOTIPY_API_REDIRECT_URI,
-                                    #scope=scope,
+                                    scope=scope,
                                     )
         token = oauth_object.get_access_token(as_dict=False)
         return token
@@ -35,6 +35,7 @@ def GetAPIObjects(token):
     genius = Genius(GENIUS_API_CLIENT_ACCESS_TOKEN)
     return spotify, genius
 
+#returns song ids vs song name/artist for better interaction with apotify api vs genius api
 def RecommendationsFromTops():
     token = GetAccessTokenWithAuthorization()
     spotify, genius = GetAPIObjects(token)
@@ -97,6 +98,70 @@ def RecommendationsFromParams(genres, danceability, popularity, tempo):
         rec_artists.append(recs_response['tracks'][i]['artists'][0]['name'])
     return recs, rec_artists
 
+#returns song ids vs song name/artist for better interaction with apotify api vs genius api
+def RecommendationIdsFromParams(genres, danceability, popularity, tempo):
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
+
+    if(danceability > 1 or danceability < 0):
+        print("invalid danceability")
+        return "invalid danceability", ""
+    
+    if(popularity > 100 or popularity < 0):
+        print("invalid popularity")
+        return "invalid popularity", ""
+    
+    if(tempo > 200 or tempo < 60):
+        print("invalid tempo")
+        return "invalid tempo", ""
+    
+    #figure out genre check!!!!
+    #all_genres = spotify.recommendation_genre_seeds()
+
+    #may want sliders or textbox for user to choose most parameters and then checkboxes for genres
+    #get recommendations based on: genre, danceabilitiy(0-1), popularity(0-100), tempo(60-200BPM)
+    
+    recs_response = spotify.recommendations(seed_genres=genres, target_danceability=danceability, target_popularity=popularity, target_tempo=tempo,limit=5, country=None)
+    recs = []
+
+    print("\nRecs from Params:")
+    for i in range(len(recs_response['tracks'])):
+        recs.append(recs_response['tracks'][i]['id'])
+        print(recs_response['tracks'][i]['name'])
+    return recs
+
+#returns song ids vs song name/artist for better interaction with apotify api vs genius api
+def RecommendationIdsFromTops():
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
+
+    # get user's top tracks and artists
+    try:
+        top_track_response = spotify.current_user_top_tracks(limit=3)
+        top_artist_response = spotify.current_user_top_artists(limit=2)
+    except spotipy.client.SpotifyException:
+        print("Error with top retrieval")
+
+    # get top artist ids and top song names/ids
+    top_artists = []
+    top_names = []
+    top_ids = []
+    for i in range(len(top_artist_response["items"])):
+        top_artists.append(top_artist_response["items"][i]["id"])
+
+    for i in range(len(top_track_response["items"])):
+        top_names.append(top_track_response["items"][i]["name"])
+        top_ids.append(top_track_response["items"][i]["id"])
+
+    # get recommendations
+    recs_response = spotify.recommendations(seed_artists=top_artists, seed_genres=None, seed_tracks=top_ids, limit=5, country=None)
+    recs = []
+    print("\n Recs from Tops:")
+    for i in range(len(recs_response['tracks'])):
+        recs.append(recs_response['tracks'][i]['id'])
+        print(recs_response['tracks'][i]['name'])
+    return recs
+
 def CommentsofRecommendations(recs, rec_artists):
     token = GetAccessTokenWithAuthorization()
     spotify, genius = GetAPIObjects(token)
@@ -136,7 +201,51 @@ def InfoOfRecommendations(recs, rec_artists):
         print(desc)
         print("___________________________________________________________________")
 
-        
+def AddRecsToLiked(recs, spotify):
+    spotify.current_user_saved_tracks_add(tracks=recs)
+
+def AddRecsToNewPlaylist(recs, spotify):
+    user_info = spotify.current_user()
+    
+    #fix to not create new every time
+    spotify.user_playlist_create(user_info['id'], "Our Recs", public=True, collaborative=False, description='recommendations from our app!')
+    
+
+    playlists_response = spotify.current_user_playlists()
+    playlists = []
+    recs_playlist = ""
+
+    for i in range(len(playlists_response['items'])):
+        playlists.append(playlists_response['items'][i]['id'])
+        if(playlists_response['items'][i]['name'] == "Our Recs"):
+            recs_playlist = playlists_response['items'][i]['id']
+
+    spotify.user_playlist_add_tracks(user_info['id'], recs_playlist, recs)
+
+
+if __name__ == "__main__":
+
+    token = GetAccessTokenWithAuthorization()
+    spotify, genius = GetAPIObjects(token)
+
+    genres = ['rock','pop','r-n-b']
+    danceability = .9
+    popularity = 50
+    tempo = 130
+    recs = RecommendationIdsFromParams(genres, danceability, popularity, tempo)
+    
+    recs2 = RecommendationIdsFromTops()
+
+    allrecs = recs + recs2
+
+    #AddRecsToLiked(allrecs, spotify)
+    AddRecsToNewPlaylist(allrecs, spotify)
+
+
+
+
+
+
 # FailedExperiment, will not test bc will not be used
 def RecommendationsFromAnnotations(song):
     token = GetAccessTokenWithAuthorization()
@@ -151,17 +260,4 @@ def RecommendationsFromAnnotations(song):
             recs.append(search_request['sections'][0]['hits'][0]['result']['title'])
 
 
-
-if __name__ == "__main__":
-   #I also made sure the song is connected to genius by both song name and author for security
-   #recommendations from attributes- can put in up to 5 genres
-    
-    genres = ['french','pop','r-n-b']
-    danceability = .9
-    popularity = 30
-    tempo = 130
-
-    recs, rec_artists = RecommendationsFromParams(genres, danceability, popularity, tempo)
-
-    InfoOfRecommendations(recs, rec_artists)
 
